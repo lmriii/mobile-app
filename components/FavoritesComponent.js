8e6ab9d381787cfb16c48c05cf806aecd12e598d
Michael R <mrwolf.lmriii@gmail.com>
2021-12-29 15:12:25 -0700

    fixed linking favorite item to it's directory page

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -27,7 +27,7 @@ class Favorites extends Component {
                     title={item.name}
                     subtitle={item.description}
                     leftAvatar={{ source: { uri: baseUrl + item.image } }}
-                    onPress={() => navigate('CampsiteInfo', { campsiteId: campsite.id })}
+                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
                 />
             );
         }
