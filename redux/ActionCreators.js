c67e2c7dd1baa0e1dad1cddfa9a0714060a30a66
Michael R <mrwolf.lmriii@gmail.com>
2021-12-29 16:28:55 -0700

    Added swipe and delete options to favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -168,3 +168,8 @@ export const addComment = newComment => ({
     type: ActionTypes.ADD_COMMENT,
     payload: newComment
 });
+
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+});
--- redux/favorites.js
+++ redux/favorites.js
@@ -1,12 +1,14 @@
 import * as ActionTypes from './ActionTypes';
 
 export const favorites = (state = [], action) => {
-    switch(action.type){
+    switch (action.type) {
         case ActionTypes.ADD_FAVORITE:
-            if(state.includes(action.payload)){
+            if (state.includes(action.payload)) {
                 return state;
             }
             return state.concat(action.payload);
+        case ActionTypes.DELETE_FAVORITE:
+            return state.filter(favorite => favorite !== action.payload);
         default:
             return state;
     }
