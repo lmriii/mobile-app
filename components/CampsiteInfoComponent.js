0e60ba07381e430dd57c026b38f862dfa8e073e0
Michael R <mrwolf.lmriii@gmail.com>
2022-01-02 23:07:06 -0700

    Workshop task 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -26,7 +26,9 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({ dx }) => (dx < -200) ? true : false;
-
+    const recognizeComment = ({ dx }) => (dx > 200) ? true : false;
+    
+ 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
@@ -52,6 +54,9 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if(recognizeComment(gestureState)){
+                console.log('we are on the right track');
+                props.onShowModal();
             }
             return true;
         }
